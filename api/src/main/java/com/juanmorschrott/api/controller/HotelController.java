77e02efa3ca85efda20edd09f16e1341ec4daaf3
juanmorschrott <juanmorschrott@outlook.es>
2020-05-28 11:02:58 +0200

    Optimize imports

--- api/src/main/java/com/juanmorschrott/api/controller/HotelController.java
+++ api/src/main/java/com/juanmorschrott/api/controller/HotelController.java
@@ -1,16 +1,25 @@
 package com.juanmorschrott.api.controller;
 
+import java.util.List;
+
 import com.juanmorschrott.api.model.Hotel;
 import com.juanmorschrott.api.service.HotelServiceImpl;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Api(value="hotels", description="Hotels simple REST API")
 @CrossOrigin(origins = "http://localhost:4200")
--- api/src/main/java/com/juanmorschrott/api/service/HotelServiceImpl.java
+++ api/src/main/java/com/juanmorschrott/api/service/HotelServiceImpl.java
@@ -1,14 +1,13 @@
 package com.juanmorschrott.api.service;
 
-import com.juanmorschrott.api.model.Hotel;
-import com.juanmorschrott.api.repository.HotelRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+import com.juanmorschrott.api.model.Hotel;
+import com.juanmorschrott.api.repository.HotelRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class HotelServiceImpl implements HotelService {
