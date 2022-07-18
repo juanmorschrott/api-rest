b835fcf54e46481c6b2d937fa1bea19e539a1c77
juanmorschrott <juanmorschrott@outlook.es>
2022-07-18 12:48:19 +0200

    Update tests

--- api/src/test/java/com/juanmorschrott/api/controller/HotelControllerWithContextIntegrationTest.java
+++ api/src/test/java/com/juanmorschrott/api/controller/HotelControllerWithContextIntegrationTest.java
@@ -1,76 +1,66 @@
 package com.juanmorschrott.api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.juanmorschrott.api.model.Hotel;
 import com.juanmorschrott.api.service.HotelServiceImpl;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Integration test with Spring Application Context using MockMvc
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(HotelController.class)
 public class HotelControllerWithContextIntegrationTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @MockBean
     private HotelServiceImpl hotelService;
 
-    private JacksonTester<Hotel> jacksonTester;
-
-    @Before
-    public void setup() {
-        JacksonTester.initFields(this, new ObjectMapper());
-    }
-
     @Test
     public void whenFindByName_thenReturnHotel() throws Exception {
         // given
-        Hotel hotel = new Hotel(1, "Foo", "Test Description", BigDecimal.valueOf(99.9));
-        given(hotelService.get(1L)).willReturn(hotel);
+        Hotel hotel = Hotel.builder()
+                .id(1)
+                .name("Foo")
+                .description("Test Description")
+                .price(BigDecimal.valueOf(99.9))
+                .build();
 
         // when
-        MockHttpServletResponse response = mvc.perform(
-                get("/api/v1/hotels/1").accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        when(hotelService.get(1L)).thenReturn(hotel);
 
         // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString()).isEqualTo(
-                jacksonTester.write(
-                        new Hotel(1, "Foo", "Test Description", BigDecimal.valueOf(99.9))).getJson()
-        );
+        mockMvc.perform(get("/api/v1/hotels/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.is(1)))
+                .andExpect(jsonPath("$.name", Matchers.is("Foo")))
+                ;
     }
 
     @Test
     public void whenFindByName_thenReturnEmptyStringIfNotExists() throws Exception {
-        // given
-        given(hotelService.get(1L)).willReturn(null);
-
         // when
-        MockHttpServletResponse response = mvc.perform(
-                get("/api/v1/hotels/1").accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+        when(hotelService.get(anyLong())).thenReturn(null);
 
         // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        mockMvc.perform(get("/api/v1/hotels/404"))
+                .andExpect(status().isNotFound());
     }
 
 }
