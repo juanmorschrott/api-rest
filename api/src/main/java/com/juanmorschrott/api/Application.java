b05ba5f97351b45cb383a4d6a7d1c53633783dfe
juanmorschrott <juanmorschrott@outlook.es>
2022-07-19 12:40:01 +0200

    Adding CORS configuration

--- api/src/main/java/com/juanmorschrott/api/Application.java
+++ api/src/main/java/com/juanmorschrott/api/Application.java
@@ -2,12 +2,28 @@ package com.juanmorschrott.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/hotels/**")
+						.allowedOriginPatterns("http://localhost:80")
+						.allowedOriginPatterns("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
