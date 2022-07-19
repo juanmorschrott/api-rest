6117850daa79616a2b383ec4246319b1d3593f03
juanmorschrott <juanmorschrott@outlook.es>
2022-07-19 12:42:40 +0200

    Add and configure frontend proxy nginx server

--- frontend/src/app/service/hotel.service.ts
+++ frontend/src/app/service/hotel.service.ts
@@ -1,31 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Hotel } from '../model/hotel.model';
 
 @Injectable()
 export class HotelService {
   
-  constructor(private http: HttpClient) { }
+  url = `${environment.api_url}/hotels`;
+  slash = '/'
 
-  uri = 'http://localhost:8080/api/v1/hotels/';
+  constructor(private http: HttpClient) {}
 
   getHotels() {
-    return this.http.get<Hotel[]>(this.uri);
+    return this.http.get<Hotel[]>(this.url);
   }
 
   getHotelById(id: number) {
-    return this.http.get(this.uri + id);
+    return this.http.get(this.url + this.slash + id);
   }
 
   createHotel(hotel: Hotel) {
-    return this.http.post(this.uri, hotel);
+    return this.http.post(this.url, hotel);
   }
 
   updateHotel(hotel: Hotel) {
-    return this.http.put(this.uri, hotel);
+    return this.http.put(this.url, hotel);
   }
 
   deleteHotel(id: number) {
-    return this.http.delete(this.uri + id);
+    return this.http.delete(this.url + this.slash + id);
   }
 }
