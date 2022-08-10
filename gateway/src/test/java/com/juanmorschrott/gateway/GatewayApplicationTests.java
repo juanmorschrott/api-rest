976c8f8eb3d63b4f553cd964aaccb454181d6710
juanmorschrott <juanmorschrott@outlook.es>
2022-08-10 11:28:32 +0200

    Adding gateway

---
+++ gateway/src/test/java/com/juanmorschrott/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmorschrott.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
