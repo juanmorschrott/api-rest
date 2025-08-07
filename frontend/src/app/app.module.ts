1c669e19ea2af8ab4065cbfded5d2e8cf2fce471
user <juanmorschrott@outlook.com>
2025-08-07 19:22:40 +0200

    Upgrade angular to v18

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,25 +7,18 @@ import { AddHotelComponent } from './add-hotel/add-hotel.component';
 import { EditHotelComponent } from './edit-hotel/edit-hotel.component';
 import { ListHotelComponent } from './list-hotel/list-hotel.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HotelService } from './service/hotel.service';
 import { DetailHotelComponent } from './detail-hotel/detail-hotel.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AddHotelComponent,
-    EditHotelComponent,
-    ListHotelComponent,
-    DetailHotelComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [HotelService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AddHotelComponent,
+        EditHotelComponent,
+        ListHotelComponent,
+        DetailHotelComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule], providers: [HotelService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
